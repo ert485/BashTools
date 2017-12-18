7d4bfb57f08d2bc2b02cc74418dff05928c64082
Erik Tetland <eriktetlandp@gmail.com>
2017-12-18 16:12:30 -0600

    c9Setup paramaters are from stdin

--- c9Setup.bash
+++ c9Setup.bash
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-
 # Usage:
-# source c9Setup.bash          # run setup in default directory
-# source c9Setup.bash dirName  # run setup in dirName directory
+    # source c9Setup.bash                       # run setup in default directory
+
+# Usage with parameters:
+    # source echo $HOME/dir | c9Setup.bash      # run setup in $HOME/dir directory
 
 defaultDir="$HOME/workspace"
 
+# Sets the directory to host from
+# parameters are from stdin, timeout of 1 second each
+# param: dir (string) the directory to host from
+#       -default is $defaultDir
 function setDir(){
-    # check if there is a first parameter
-    if [ "$#" -gt 0 ]; then
-        # check for absolute path
-        if [[ "$1" = /* ]]; then
-            dir="$1"
-        else
-            dir="$PWD/$1"
-        fi
-    else
-        # no parameter, use default
+    # parameters are from stdin, timeout of 1 second each
+    echo "Enter the directory to host from"
+    read -t 1 dir
+    if [ -z "$dir" ]; then
+        echo "timeout, using default"
         dir="$defaultDir"
     fi
+    # check for absolute path
+    if [[ "$dir" = /* ]]; then
+        dir="$dir"
+    else
+        dir="$PWD/$dir"
+    fi
+    echo "Using directory: $dir"
 }
 
 # logs results to file
@@ -27,11 +34,11 @@ function initLogs(){
     mkdir -p "$dir/logs"
     logFile="$dir/logs/c9Setup.log.txt"
     serveLogFile="$dir/logs/serve.log.txt"
-    
-    # add timestamp to logs
+  # get year-month-day Hour:Minute:Second timestamp
     timestamp() {
       date +"%Y-%m-%d %H:%M:%S"
     }
+  # add timestamp to each log
     echo "<-> Running c9Setup.bash in $dir: $(timestamp)" >> $logFile >> $serveLogFile
     echo "<-> Running c9Setup.bash in $dir: $(timestamp)" >> $logFile >> $logFile
 }
@@ -40,8 +47,9 @@ function initLogs(){
 # param $1 (string) search term to look for repos
 # post condition: repos matching $1 will be updated
 update_linux_repo() {
-    # find repos containing the parameter (string)
+  # find repos containing the parameter (string)
     repos=$(grep -rl "$1" /etc/apt/sources.list.d)
+  # update each repo
     for repo in $repos;
     do
         sudo apt-get update -o Dir::Etc::sourcelist="$repo" -o Dir::Etc::sourceparts="-"
@@ -50,19 +58,17 @@ update_linux_repo() {
 
 # gets php dependencies that are required for Laravel
 function installPHPdependencies(){
-    # add repo
+  # add repo
     sudo add-apt-repository -y ppa:ondrej/php 
-    # update repo
+  # update repo
     update_linux_repo php
-    
-    # install php packages
+  # install php packages
     sudo apt-get install -y libapache2-mod-php7.1
     sudo apt-get install -y php7.1-dom
     sudo apt-get install -y php7.1-mbstring
     sudo apt-get install -y php7.1-zip
     sudo apt-get install -y php7.1-mysql 
-    
-    # switch apache to using php7.1 instead of php5
+  # switch apache to using php7.1 instead of php5
     sudo a2dismod php5
     sudo a2enmod php7.1
 } 
@@ -74,15 +80,16 @@ function setSiteConf(){
     apacheSitesDir="/etc/apache2/sites-available"
     oldHost="/home/ubuntu/workspace"
     newHost="$dir/public"
-    #copy site .conf file
+  #copy site .conf file
     sudo cp "$apacheSitesDir/$copyFromConf" "$apacheSitesDir/$newConfName"
-    #change the site to be hosted from the "public" folder
+  #change the site to be hosted from the "public" folder
     sudo sed -i "s|$oldHost|$newHost|g" "$apacheSitesDir/$newConfName"
-    #set the correct site to enabled, disable others
+  #set the correct site to enabled, disable others
     p=$PWD
     cd /etc/apache2/sites-enabled
     sudo a2dissite -q *; sudo a2ensite -q $newConfName
     cd $p
+    echo $dir
 }
 
 # gets laravel installer
@@ -95,6 +102,7 @@ function installLaravelDependencies(){
 # Start calling functions
 
 setDir
+
 initLogs
 
 echo "<-> Installing php dependencies"
@@ -104,7 +112,7 @@ echo "<-> Configuring Site"
 setSiteConf &>> $logFile 
 
 echo "<-> Serve at https://$C9_PROJECT-$C9_USER.c9users.io"
-run-apache2 &>> $serveLogFile &
+run-apache2 &>> $serveLogFile &                         # runs in background
 
 echo "<-> Installing Laravel dependencies"
 installLaravelDependencies &>> $logFile
