2c90b80b5dfd0c3a5e409f50fd440d3405e4d358
Erik Tetland <eriktetlandp@gmail.com>
2017-12-30 11:42:39 -0600

    NOT TESTED sets apache config

--- laravelSetup.bash
+++ laravelSetup.bash
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-MYSQL_PASS="f8v93-c2$D"
+PROJECT_NAME="test"
+MYSQL_DATABASE="laravel"
+MYSQL_PASS="secret58bh"
+ADMIN_EMAIL=""
+DOMAIN_NAME=""
+
+DIR="/home/$PROJECT_NAME"
+
+#----------------------------
+# Helper functions:
 
 # updates any linux repo that contains $1 in the .list filename
 # param $1 (string) search term to look for repos
@@ -29,6 +38,35 @@ function installPHPdependencies(){
     sudo apt-get install -y php7.1-mysql 
 } 
 
+# sets configs to serve from the appropriate directory
+function setApacheConf(){
+    newConfName="$PROJECT_NAME.conf"
+    apacheSitesDir="/etc/apache2/sites-available"
+    conf="$apacheSitesDir/$newConfName"
+    
+    newRoot="$DIR/public"
+    
+    echo '<VirtualHost *:80>'                 > $conf
+    echo -e "\tServerName $DOMAIN_NAME"       >> $conf
+    echo -e "\tServerAdmin $ADMIN_EMAIL"      >> $conf
+    echo -e "\tDocumentRoot" $newRoot         >> $conf
+    echo -e "\tLogLevel info"                 >> $conf
+    echo -e "\tErrorLog ${APACHE_LOG_DIR}/error.log" >> $conf
+    echo -e "\tCustomLog ${APACHE_LOG_DIR}/access.log combined" >> $conf
+    echo -e "\t" '<Directory' " $newRoot" '>' >> $conf
+    echo -e "\t\tOptions Indexes FollowSymLinks" >> $conf
+    echo -e "\t\tAllowOverride All\n\t\tRequire all granted" >> $conf
+    echo -e "\t" '</Directory>'               >> $conf
+    echo -e '</VirtualHost>'                  >> $conf
+    
+  #enable the site, disable default
+    sudo a2dissite 000-default; sudo a2ensite $newConfName
+    sudo service apache2 restart
+}
+
+#----------------------------
+# Start executing 
+
 sudo apt update
 
 # from https://gist.github.com/sheikhwaqas/9088872
@@ -36,5 +74,11 @@ sudo apt update
 echo "mysql-server mysql-server/root_password password $MYSQL_PASS" | sudo debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $MYSQL_PASS" | sudo debconf-set-selections
 sudo apt-get -y install mysql-server
+
+mysql --user="root" --password="$MYSQL_PASS" --execute="create database $MYSQL_DATABASE"
+
+apt install apache2
+
+sudo a2enmod rewrite
 
 installPHPdependencies
